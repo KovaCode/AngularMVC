f89865498e986527184ca32c5473624584020da9
Ivan kovacic <ivan.kovacic@fiveminutes.eu>
2018-02-26 13:14:16 +0100

    Add project files.

---
+++ AngularMVC/js/controller/makeController.js
@@ -0,0 +1,24 @@
+﻿angular.module('myApp')
+
+    .controller('MakeController', ['$scope', 'makeService', function ($scope, makeService) {
+        $scope.getAllMake = function () {
+            makeService.getAllMake()
+            .then(function (response) {
+                $scope.makes = response.data;
+                $scope.error = null;
+            }, function (error) {
+                console.log(error, 'can not get data.');
+                $scope.makes = null;
+                $scope.error = "can not get data";
+                
+            });
+
+                //.success(function (response) {
+                //    $scope.makes = response.records;
+                //    $scope.error = null;
+                //}).error(function () {
+                //    $scope.makes = null;
+                //    $scope.error = 'Server communication error';
+                //});
+        };
+    }]);
---
+++ AngularMVC/js/service/makeService.js
@@ -0,0 +1,8 @@
+﻿angular.module('myApp')
+    .service('makeService', ['$http', '$q', function ($http, $q) {
+
+        this.getAllMake = function () {
+            return $http.get('http://localhost/WebApi/api/make');
+        };
+
+    }]);
